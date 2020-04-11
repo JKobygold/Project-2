0c6c527546f9e7c5401567e1d275ff21b6a65fe8
Ben Mulholland <ben.mulholland@gmail.com>
2020-04-11 13:58:23 -0400

    added db files needs testing and resetting depending on other values in page

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,18 @@
-DROP DATABASE IF EXISTS Exampledb;
-CREATE DATABASE Exampledb;
+-- DROP DATABASE IF EXISTS Exampledb;
+-- CREATE DATABASE Exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- DROP DATABASE IF EXISTS testdb;
+-- CREATE DATABASE testdb;
+
+### Schema
+
+CREATE DATABASE Pandemicplannerdb;
+USE Pandemicplannerdb;
+
+CREATE TABLE pplanner
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	cabin-fever BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
