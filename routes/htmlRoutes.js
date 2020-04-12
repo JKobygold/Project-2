6ea161901a562b798b8ca395587ab3613585d4a1
Jacob Gold <jkobygold@gmail.com>
2020-04-11 21:12:15 -0400

    some stuffs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,6 +26,13 @@ module.exports = function(app) {
     });
   });
 
+app.get("/another", function(req,res) {
+  res.send("index");
+  });
+
+
+
+    
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
